5b14800a4c0db6d4f8e5e0f771cf817398024708
Rasel Ahmed <raselahmed.lucse@gmail.com>
2021-05-16 03:55:10 +0100

    first commit with initial demo

---
+++ js/dash-charts.js
@@ -0,0 +1,133 @@
+﻿/*** First Chart in Dashboard page ***/
+
+	$(document).ready(function() {
+		info = new Highcharts.Chart({
+			chart: {
+				renderTo: 'steps',
+				margin: [0, 0, 0, 0],
+				backgroundColor: null,
+                plotBackgroundColor: 'none',
+							
+			},
+			
+			title: {
+				text: "Steps",
+				color: '#F1F3EB'
+			},
+
+			tooltip: {
+				formatter: function() { 
+					return this.point.name +': '+ this.y +' %';
+						
+				} 	
+			},
+		    series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'Steps',
+				innerSize: '65%',
+				data: [
+					{ name: 'Completed Steps', y: 45.0, color: '#b2c831' },
+					{ name: 'Rest Steps', y: 55.0, color: '#F1F3EB' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]
+		});
+		
+	});
+
+/*** second Chart in Dashboard page ***/
+
+	$(document).ready(function() {
+		info = new Highcharts.Chart({
+			chart: {
+				renderTo: 'cal',
+				margin: [0, 0, 0, 0],
+				backgroundColor: null,
+                plotBackgroundColor: 'none',
+							
+			},
+			
+			title: {
+				text: "Distance"
+			},
+
+			tooltip: {
+				formatter: function() { 
+					return this.point.name +': '+ this.y +' %';
+						
+				} 	
+			},
+		    series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'Distance(km)',
+				innerSize: '65%',
+				data: [
+					{ name: 'Completed distance', y: 65.0, color: '#34eb83' },
+					{ name: 'Rest of distance', y: 35.0, color: '#F1F3EB' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]
+		});
+		
+	});
+
+	/*** Third Chart in Dashboard page ***/
+
+	$(document).ready(function() {
+		info = new Highcharts.Chart({
+			chart: {
+				renderTo: 'space',
+				margin: [0, 0, 0, 0],
+				backgroundColor: null,
+                plotBackgroundColor: 'none',
+							
+			},
+			
+			title: {
+				text: "Sleep"
+			},
+
+			tooltip: {
+				formatter: function() { 
+					return this.point.name +': '+ this.y +' %';
+						
+				} 	
+			},
+		    series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'Sleep Time',
+				innerSize: '65%',
+				data: [
+					{ name: 'Completed Time(Mins)', y: 97.0, color: '#fa1d2d' },
+					{ name: 'Rest of Time(Mins)', y: 3.0, color: '#F1F3EB' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]
+		});
+		
+	});
+
